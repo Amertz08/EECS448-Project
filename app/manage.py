a7e53ea532e6300507cbf3603fbd5bdcc6e12e7c
Adam Mertz <adammertz@gmail.com>
2017-03-29 09:41:49 -0500

    Integrated migration. Added Shell context. Added testing a createdb commands.

--- app/manage.py
+++ app/manage.py
@@ -1,13 +1,35 @@
 from __future__ import unicode_literals, print_function, absolute_import
 import os
 
-from flask_script import Manager
+from flask_script import Manager, Shell
+from flask_migrate import MigrateCommand
+import pytest
 
 from create import create_app
+from models import db
 
 application = create_app(os.getenv('APP_CONFIG') or 'default')
 
+
+def make_shell_context():
+    return dict(
+        app=application, db=db
+    )
+
 manager = Manager(application)
+manager.add_command('db', MigrateCommand)
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+
+@manager.command
+def createdb():
+    db.create_all()
+
+
+@manager.command
+def test():
+    pytest.main()
+
 
 if __name__ == '__main__':
     manager.run()
