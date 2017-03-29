e0c647fdb8162b1b3acec77b02c8d88a3f1fdb95
Adam Mertz <adammertz@gmail.com>
2017-03-29 08:48:51 -0500

    Added initial index template.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals, print_function, absolute_import
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
-    return 'hello'
+    return render_template('index.html')
