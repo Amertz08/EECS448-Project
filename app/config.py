3a3652872a6cf91b120957e1e25e46071cb87fa4
Adam Mertz <adammertz@gmail.com>
2017-03-29 08:45:51 -0500

    Integrated config from object with multiple configuration states.

---
+++ app/config.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals, print_function, absolute_import
+
+import os
+
+
+class Config(object):
+    SECRET_KEY = os.getenv('SECRET_KEY') or '))aos47#z9%t&fwx=jqcf+yo9&&7s&(+4@(gt9gl70i-_4mh*p'
+
+    MYSQL_USER = 'EECS448'
+    MYSQL_PASS = 'z81mL2eJgK5y'
+    MYSQL_DB = 'EECS448_dev'
+    MYSQL_HOST = 'localhost'
+
+    # Database info
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{usr}:{passwd}@{host}/{db}'.format(
+        usr=MYSQL_USER, passwd=MYSQL_PASS, host=MYSQL_HOST, db=MYSQL_DB
+    )
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    PRODUCTION = True
+
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
