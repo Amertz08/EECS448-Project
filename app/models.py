e30abca16df1a0a6cd18eeaf9be3f635056e1d1d
HHuang <huanghaozhan@gmail.com>
2017-03-31 16:17:59 -0500

    Made basic User model

--- app/models.py
+++ app/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals, print_function, division, absolute_impo
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255), default='')
+    last_name = db.Column(db.String(255), default='')
+    email = db.Column(db.String(255), unique=True, default='')
