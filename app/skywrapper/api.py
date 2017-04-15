c47b01a31833bda7cfb95305d2bfb7c72d3d2979
Adam Mertz <adammertz@gmail.com>
2017-04-15 10:49:20 -0500

    Added update results function to refresh results of initial query

--- app/skywrapper/api.py
+++ app/skywrapper/api.py
@@ -7,12 +7,24 @@ from skywrapper.models import Query, QueryResults
 
 
 class LiveFlights(Flights):
+    params = None
+
     def query_flights(self, params):
         """
-
+        Returns QueryResults object with results data
         :param params: 
         :return: 
         """
+        self.params = params
         query = Query(**params)
         response = self.get_result(**query.serialize)
         return QueryResults(query, response)
+
+    def update_result(self):
+        """
+        Returns QueryResults object with updated data
+        :return: 
+        """
+        if self.params is None:
+            raise AttributeError('params not set')
+        return self.query_flights(self.params)
