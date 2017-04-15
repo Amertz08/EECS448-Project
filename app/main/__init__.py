327d23315261a3a23a92324be6b2806ce517382a
Adam Mertz <adammertz@gmail.com>
2017-04-14 22:56:34 -0500

    Initial writing of wrapper module

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -5,7 +5,7 @@ import arrow
 from flask import Blueprint, render_template
 
 from forms import SearchForm
-from skyAPI import flight_service
+from skyAPI import flight_service, live_flights
 
 main = Blueprint('main', __name__)
 
@@ -16,18 +16,16 @@ def index():
     results = None
     if form.validate_on_submit():
         kwargs = {
-            'country': 'US',
-            'currency': 'USD',
-            'locale': 'en-US',
             'originplace': form.origin_place_id.data,
             'destinationplace': form.destination_place_id.data,
             'inbounddate': arrow.get(form.inbound_date.data).format('YYYY-MM-DD'),
             'outbounddate': arrow.get(form.outbound_date.data).format('YYYY-MM-DD'),
             'adults': form.adults.data,
             'children': form.children.data,
-            'infants': form.infants.data
+            'infants': form.infants.data,
         }
-        results = flight_service.get_result(**kwargs).json()
+        results = live_flights.query_flights(kwargs)
+        print(results.serialize)
 
     context = {
         'form': form,
